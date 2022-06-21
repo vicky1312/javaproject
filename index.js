10619ad0869858e6c07001173d4e6ca2541add31
vicky1312 <victoryabayomi@gmail.com>
2022-06-21 16:19:07 +0100

    jav project

---
+++ index.js
@@ -0,0 +1,5 @@
+let myname = 'Victory';
+let myheight = '5ft 2inches';
+let mycountry = 'Nigeria';
+document.write("My name is" + " " + myname +"<br>" + "My height is" + " "+ myheight + "<br>" + "I come from " 
++ " " + mycountry)
